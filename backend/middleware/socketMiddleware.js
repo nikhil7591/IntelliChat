1577adf5fd9065900f3137569cd3c71ffbc33c3f
Nikhil kumar <nikhil759100@gmail.com>
2025-11-03 10:00:09 +0530

    resolve error

--- backend/middleware/socketMiddleware.js
+++ backend/middleware/socketMiddleware.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 
 const socketMiddleware = (socket, next) => {
  
-  const token = socket.handshake.auth.token || socket.handshake.headers["authorization"].split(' ')[1];
+  const token = socket.handshake.auth?.token || socket.handshake.headers["authorization"].split(' ')[1];
   if (!token ) {
     return next(new Error("Authorization token missing. Please provide token"));
   }
